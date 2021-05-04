e65b3b24a57ecc071504f16f01d917ea6237b51d
mohdarshad1 <mdarshad181@gmail.com>
2021-05-04 21:46:26 +0530

    [Mohd Arshad] : Refactor Refactor1 Generic Method

--- lib/src/main/java/com/TestMaximum/FindMaximum.java
+++ lib/src/main/java/com/TestMaximum/FindMaximum.java
@@ -1,32 +1,15 @@
 package com.TestMaximum;
 
 public class FindMaximum 
-{
-	public static Integer maximumInteger(Integer a, Integer b, Integer c) {
-		Integer maximumValue = a;
-		if (b.compareTo(maximumValue) > 0)
-			maximumValue = b;
-		if (c.compareTo(maximumValue) > 0)
-			maximumValue = c;
-		return maximumValue;
-	}
-
-	public static Float maximumFloat(  Float d, Float e, Float f) {
-		Float maximumValue = d;
-		if (e.compareTo(maximumValue) > 0)
-			maximumValue = e;
-		if (f.compareTo(maximumValue) > 0)
-			maximumValue = f;
-		System.out.println(maximumValue);
-		return maximumValue;
-	}
-	
-	public static String maximumString(  String g, String h,String i) {
-		String maximumValue = g;
-		if (h.compareTo(maximumValue) > 0)
-			maximumValue = h;
-		if (i.compareTo(maximumValue) > 0)
-			maximumValue = i;
+{	
+	// Generic Method
+	public static <T extends Comparable<T>> T maximumOfObject(T first, T second, T third) {
+		T maximumValue = first; 
+		if (second.compareTo(maximumValue) > 0)
+			maximumValue = second;
+		if (third.compareTo(maximumValue) > 0)
+			maximumValue = third; 
+		
 		System.out.println(maximumValue);
 		return maximumValue;
 	}
