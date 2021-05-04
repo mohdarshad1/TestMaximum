e309de7564e861da7cc40895715f78d6ef9331b9
mohdarshad1 <mdarshad181@gmail.com>
2021-05-04 21:41:20 +0530

    [Mohd Arshad] : Add Max at Postion 1, 2 & 3 (Float)

--- lib/src/main/java/com/TestMaximum/FindMaximum.java
+++ lib/src/main/java/com/TestMaximum/FindMaximum.java
@@ -2,6 +2,7 @@ package com.TestMaximum;
 
 public class FindMaximum 
 {
+	
 	public static Integer maximumInteger(Integer a, Integer b, Integer c) {
 		Integer maximumValue = a;
 		if (b.compareTo(maximumValue) > 0)
@@ -9,5 +10,20 @@ public class FindMaximum
 		if (c.compareTo(maximumValue) > 0)
 			maximumValue = c;
 		return maximumValue;
+		
 	}
+	
+
+	public static Float maximumFloat(  Float d, Float e, Float f) {
+		Float maximumValue = d;
+		if (e.compareTo(maximumValue) > 0)
+			maximumValue = e;
+		if (f.compareTo(maximumValue) > 0)
+			maximumValue = f;
+		System.out.println(maximumValue);
+		return maximumValue;
+		
+		
+	}
+
 }
--- lib/src/test/java/com/TestMaximum/FindMaximumTest.java
+++ lib/src/test/java/com/TestMaximum/FindMaximumTest.java
@@ -4,25 +4,39 @@ import org.junit.Assert;
 import org.junit.Test;
 public class FindMaximumTest {
 	
-
-	
 	@Test
 	public void maximumIntegerAtPosition1() {
 		Integer maximumInteger = FindMaximum.maximumInteger(12, 4, 11);
 		Assert.assertSame(12, maximumInteger);
 	}
-
 	
 	@Test
 	public void maximumIntegerAtPosition2() {
 		Integer maximumInteger = FindMaximum.maximumInteger(4, 12, 11);
 		Assert.assertSame(12, maximumInteger);
 	}
-
 	
 	@Test
 	public void maximumIntegerAtPosition3() {
 		Integer maximumInteger = FindMaximum.maximumInteger(4, 11, 12);
 		Assert.assertSame(12, maximumInteger);
+	}
+	
+	@Test
+	public void maximumFloatAtPosition1() {
+		Float maximumFloat = FindMaximum.maximumFloat(4.2f, 3.1f, 1.5f);
+		Assert.assertEquals((Float) 4.2f, maximumFloat);
+	}
+	
+	@Test
+	public void maximumFloatAtPosition2() {
+		Float maximumFloat = FindMaximum.maximumFloat(3.1f, 4.2f, 1.5f);
+		Assert.assertEquals((Float) 4.2f, maximumFloat);
+	}
+
+	@Test
+	public void maximumFloatAtPosition3() {
+		Float maximumFloat = FindMaximum.maximumFloat(1.5f, 3.1f, 4.2f);
+		Assert.assertEquals((Float) 4.2f, maximumFloat);
 	}
 }
