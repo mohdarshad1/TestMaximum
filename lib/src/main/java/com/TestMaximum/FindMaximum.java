02a5e115004a0774017d000f6a725f0ef983b34e
mohdarshad1 <mdarshad181@gmail.com>
2021-05-04 21:43:21 +0530

    [Mohd Arshad] : Add Max at Postion 1, 2 & 3 (String)

--- lib/src/main/java/com/TestMaximum/FindMaximum.java
+++ lib/src/main/java/com/TestMaximum/FindMaximum.java
@@ -2,7 +2,6 @@ package com.TestMaximum;
 
 public class FindMaximum 
 {
-	
 	public static Integer maximumInteger(Integer a, Integer b, Integer c) {
 		Integer maximumValue = a;
 		if (b.compareTo(maximumValue) > 0)
@@ -10,9 +9,7 @@ public class FindMaximum
 		if (c.compareTo(maximumValue) > 0)
 			maximumValue = c;
 		return maximumValue;
-		
 	}
-	
 
 	public static Float maximumFloat(  Float d, Float e, Float f) {
 		Float maximumValue = d;
@@ -22,8 +19,15 @@ public class FindMaximum
 			maximumValue = f;
 		System.out.println(maximumValue);
 		return maximumValue;
-		
-		
 	}
-
+	
+	public static String maximumString(  String g, String h,String i) {
+		String maximumValue = g;
+		if (h.compareTo(maximumValue) > 0)
+			maximumValue = h;
+		if (i.compareTo(maximumValue) > 0)
+			maximumValue = i;
+		System.out.println(maximumValue);
+		return maximumValue;
+	}
 }
--- lib/src/test/java/com/TestMaximum/FindMaximumTest.java
+++ lib/src/test/java/com/TestMaximum/FindMaximumTest.java
@@ -9,13 +9,13 @@ public class FindMaximumTest {
 		Integer maximumInteger = FindMaximum.maximumInteger(12, 4, 11);
 		Assert.assertSame(12, maximumInteger);
 	}
-	
+
 	@Test
 	public void maximumIntegerAtPosition2() {
 		Integer maximumInteger = FindMaximum.maximumInteger(4, 12, 11);
 		Assert.assertSame(12, maximumInteger);
 	}
-	
+
 	@Test
 	public void maximumIntegerAtPosition3() {
 		Integer maximumInteger = FindMaximum.maximumInteger(4, 11, 12);
@@ -27,7 +27,7 @@ public class FindMaximumTest {
 		Float maximumFloat = FindMaximum.maximumFloat(4.2f, 3.1f, 1.5f);
 		Assert.assertEquals((Float) 4.2f, maximumFloat);
 	}
-	
+
 	@Test
 	public void maximumFloatAtPosition2() {
 		Float maximumFloat = FindMaximum.maximumFloat(3.1f, 4.2f, 1.5f);
@@ -38,5 +38,23 @@ public class FindMaximumTest {
 	public void maximumFloatAtPosition3() {
 		Float maximumFloat = FindMaximum.maximumFloat(1.5f, 3.1f, 4.2f);
 		Assert.assertEquals((Float) 4.2f, maximumFloat);
+	}
+	
+	@Test
+	public void maximumStringAtPosition1() {
+		String maximumString = FindMaximum.maximumString("Peach", "Apple", "Banana");
+		Assert.assertEquals("Peach", maximumString);
+	}
+
+	@Test
+	public void maximumStringAtPosition2() {
+		String maximumString = FindMaximum.maximumString("Apple", "Peach", "Banana");
+		Assert.assertEquals("Peach", maximumString);
+	}
+
+	@Test
+	public void maximumStringAtPosition3() {
+		String maximumString = FindMaximum.maximumString("Apple", "Banana", "Peach");
+		Assert.assertEquals("Peach", maximumString);
 	}
 }
